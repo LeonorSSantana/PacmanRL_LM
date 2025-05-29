5c67826de16b4c88a088f79356cb6ef0264ff2db
Matilde Alves <matilde.grilo.alves@gmail.com>
2025-05-29 20:03:01 +0100

    ve se mudou

--- agents/q_learning.py
+++ agents/q_learning.py
@@ -1,4 +1,7 @@
 import pickle
+import random
+import numpy as np
+
 
 
 class QLearning:
@@ -11,8 +14,29 @@ class QLearning:
         self.min_epsilon = min_epsilon  # Minimum epsilon value
         self.q_table = {}  # Q-table for storing state-action values
 
+    def state_to_key(self, state):
+        if isinstance(state, np.ndarray):
+            return tuple(state.flatten())
+        elif isinstance(state, (list, tuple)):
+            # Recursively convert elements inside list/tuple
+            return tuple(self.state_to_key(s) for s in state)
+        elif isinstance(state, dict):
+            # Recursively convert dict items
+            return tuple((k, self.state_to_key(v)) for k, v in sorted(state.items()))
+        else:
+            return (state,)
+
+
     def get_action(self, state):
-        pass
+        state_key = self.state_to_key(state)
+        if random.uniform(0, 1) < self.epsilon:
+            return self.env.action_space.sample()
+        else:
+            state_actions = self.q_table.get(state_key, {})
+            if not state_actions:
+                return self.env.action_space.sample()
+            return max(state_actions, key=state_actions.get)
+
 
     def decay_epsilon(self):
         """
@@ -26,14 +50,67 @@ class QLearning:
         :param num_episodes: Number of episodes to train the agent for.
         :return: Q-table with learned state-action values.
         """
-        pass
+        for episode in range(num_episodes):
+            obs = self.env.reset()
+            state = obs[0] if isinstance(obs, tuple) else obs
+            state_key = self.state_to_key(state)
+
+            done = False
+
+            while not done:
+                action = self.get_action(state)
+
+                next_state, reward, terminated, truncated, _ = self.env.step(action)
+                next_state_key = self.state_to_key(next_state)
+                done = terminated or truncated
+
+                if state_key not in self.q_table:
+                    self.q_table[state_key] = {}
+                if action not in self.q_table[state_key]:
+                    self.q_table[state_key][action] = 0.0
+                if next_state_key not in self.q_table:
+                    self.q_table[next_state_key] = {}
+
+                next_max = max(self.q_table[next_state_key].values()) if self.q_table[next_state_key] else 0.0
+                old_value = self.q_table[state_key][action]
+                self.q_table[state_key][action] = old_value + self.alpha * (reward + self.gamma * next_max - old_value)
+
+                state = next_state
+                state_key = next_state_key
+
+            self.decay_epsilon()
+
+        return self.q_table
+
+
 
     def test(self, num_episodes=100):
         """
         Run the environment using the learned policy to evaluate performance.
         :param num_episodes: Number of episodes to run for testing.
         """
-        pass
+        self.set_epsilon_to_min()
+
+        total_rewards = []
+
+        for episode in range(num_episodes):
+            state = self.env.reset()[0]
+            done = False
+            episode_reward = 0
+
+            while not done:
+                action = self.get_action(state)
+                next_state, reward, terminated, truncated, _ = self.env.step(action)
+                done = terminated or truncated
+                episode_reward += reward
+                state = next_state
+
+            total_rewards.append(episode_reward)
+
+        avg_reward = np.mean(total_rewards)
+        print(f"Recompensa média durante o teste: {avg_reward}")
+
+
 
     def save_q_table(self, filename='models/q_learning.pkl'):
         """
--- agents/sarsa.py
+++ agents/sarsa.py
@@ -1,4 +1,6 @@
 import pickle
+import random
+import numpy as np
 
 
 class SARSA:
@@ -11,8 +13,25 @@ class SARSA:
         self.min_epsilon = min_epsilon  # Minimum epsilon value
         self.q_table = {}  # Q-table for storing state-action values
 
+    def state_to_key(self, state):
+        if isinstance(state, np.ndarray):
+            return tuple(state.flatten())
+        elif isinstance(state, (list, tuple)):
+            return tuple(self.state_to_key(s) for s in state)
+        elif isinstance(state, dict):
+            return tuple((k, self.state_to_key(v)) for k, v in sorted(state.items()))
+        else:
+            return (state,)
+
     def get_action(self, state):
-        pass
+        state_key = self.state_to_key(state)
+        if random.uniform(0, 1) < self.epsilon:
+            return self.env.action_space.sample()
+        else:
+            state_actions = self.q_table.get(state_key, {})
+            if not state_actions:
+                return self.env.action_space.sample()
+            return max(state_actions, key=state_actions.get)
 
     def decay_epsilon(self):
         """
@@ -27,14 +46,64 @@ class SARSA:
         :param num_episodes: Number of episodes to train the agent for.
         :return: Q-table with learned state-action values.
         """
-        pass
+        for episode in range(num_episodes):
+            state = self.env.reset()[0]
+            state_key = self.state_to_key(state)
+            action = self.get_action(state)
+            done = False
+
+            while not done:
+                next_state, reward, terminated, truncated, _ = self.env.step(action)
+                done = terminated or truncated
+                next_state_key = self.state_to_key(next_state)
+                next_action = self.get_action(next_state)
+
+                # Inicializa os dicionários se necessário
+                if state_key not in self.q_table:
+                    self.q_table[state_key] = {}
+                if action not in self.q_table[state_key]:
+                    self.q_table[state_key][action] = 0.0
+                if next_state_key not in self.q_table:
+                    self.q_table[next_state_key] = {}
+                if next_action not in self.q_table[next_state_key]:
+                    self.q_table[next_state_key][next_action] = 0.0
+
+                # Atualiza a Q-table com a fórmula SARSA
+                old_value = self.q_table[state_key][action]
+                next_value = self.q_table[next_state_key][next_action]
+                self.q_table[state_key][action] = old_value + self.alpha * (reward + self.gamma * next_value - old_value)
+
+                state_key = next_state_key
+                action = next_action
+
+            self.decay_epsilon()
+
+        return self.q_table
 
     def test(self, num_episodes=100):
         """
         Run the environment using the learned policy to evaluate performance.
         :param num_episodes: Number of episodes to run for testing.
         """
-        pass
+        self.set_epsilon_to_min()
+        total_rewards = []
+
+        for episode in range(num_episodes):
+            state = self.env.reset()[0]
+            done = False
+            episode_reward = 0
+
+            while not done:
+                action = self.get_action(state)
+                next_state, reward, terminated, truncated, _ = self.env.step(action)
+                done = terminated or truncated
+                episode_reward += reward
+                state = next_state
+
+            total_rewards.append(episode_reward)
+
+        avg_reward = np.mean(total_rewards)
+        print(f"Recompensa média durante o teste: {avg_reward}")
 
     def save_q_table(self, filename='models/sarsa.pkl'):
         """
